a543bc96055efa4b6384dd67b02f1b419678f9ba
LawrenceXavier <lawrencealgorithm@gmail.com>
2018-01-07 23:23:38 +0700

    Edit settings button position and type

--- src/main/java/com/example/lawrence/nmcnpm/SwipeScreenActivity.java
+++ src/main/java/com/example/lawrence/nmcnpm/SwipeScreenActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SwipeScreenActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class SwipeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_screen);
 
-        Button settingsButton = findViewById(R.id.SwipeScreen_buttonSettings);
+        ImageButton settingsButton = findViewById(R.id.SwipeScreen_buttonSettings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
